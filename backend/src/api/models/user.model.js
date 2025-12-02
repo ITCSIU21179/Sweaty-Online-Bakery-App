bfa1cef5d103d1a83cd04ff55898d823c73f9887
ITCSIU21192 Ngo I Khang <ikhang2003@gmail.com>
2025-12-02 22:14:43 +0700

    auth & user api

---
+++ backend/src/api/models/user.model.js
@@ -0,0 +1,87 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            validate: {
+                validator: validator.isEmail,
+                message: (props) => `${props.value} is not a valid email!`,
+            },
+            required: [true, "User email required"],
+            immutable: true,
+            index: true,
+        },
+        phoneNumber: {
+            type: String,
+            validate: {
+                validator: function (v) {
+                    return validator.isMobilePhone(v, "any", {
+                        strictMode: false,
+                    });
+                },
+                message: (props) =>
+                    `${props.value} is not a valid phone number!`,
+            },
+            unique: true,
+            required: [true, "User phone number required"],
+        },
+        fullName: {
+            type: String,
+            trim: true,
+            minlength: [2, "Full name must be at least 2 characters"],
+            maxlength: [50, "Full name cannot exceed 50 characters"],
+            required: [true, "User full name required"],
+        },
+        password: {
+            type: String,
+            required: [true, "User password is required"],
+            minlength: [6, "Password must be at least 6 characters"],
+            validate: {
+                validator: function (password) {
+                    const hasUpper = /[A-Z]/.test(password);
+                    const hasLower = /[a-z]/.test(password);
+                    const hasNumber = /\d/.test(password);
+                    const hasSpecial = /[^A-Za-z0-9]/.test(password);
+
+                    return hasUpper && hasLower && hasNumber && hasSpecial;
+                },
+                message:
+                    "Password must contain uppercase, lowercase, number, and special character",
+            },
+        },
+        role: {
+            type: String,
+            enum: ["customer", "admin"],
+            default: "customer",
+            required: [true, "User needs a role"],
+        },
+        address: {
+            type: String,
+            default: "",
+        },
+        lastLogin: {
+            type: Date,
+            default: Date.now,
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        resetPasswordToken: String,
+        resetPasswordExpiresAt: Date,
+        verificationToken: String,
+        verificationTokenExpiresAt: Date,
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
